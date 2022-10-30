2ae36a779fcc7cee34a87cdb5f42b352f4735d8f
laura-macbook <git@isadora.de>
2022-10-30 23:22:05 +0100

    final touches

--- src/game2048/logic/Dimensions.scala
+++ src/game2048/logic/Dimensions.scala
@@ -1,9 +1,6 @@
 package game2048.logic
 
-// you can alter this file!
-
 case class Dimensions(width : Int, height : Int) {
-  // scanned from left to right, top to bottom
   def allPointsInside : Seq[Point] =
     for(y <- 0 until height; x <- 0 until width)
       yield Point(x,y)
